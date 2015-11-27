2b271155ae8b61c3afeb9d5da674d364ed5eca88
Sérgio Umlauf <sergio.umlauf@mosaicco.com>
2015-11-27 16:40:09 -0200

    Mais algumas alterações no carrinho.

--- routes/carrinho.js
+++ routes/carrinho.js
@@ -4,15 +4,25 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-      res.render('carrinho/index');
+  res.render('carrinho/index');
 
 });
 
 
 router.get('/:produto_id', function(req, res, next) {
-
-      res.render('carrinho/index', { produto: req.params.produto_id });
-
+  if(!req.session.carrinho) {
+    req.session.carrinho = { items:[] };
+    req.session.carrinho.items.push({ produto_id:req.params.produto_id, qtde: 1 });
+  } else {
+    // Só adiciona o produto se ainda não existir no carrinho.
+    var existe = req.session.carrinho.items.some(function(item) {
+      return item.produto_id == req.params.produto_id;
+    });
+    if(!existe) {
+      req.session.carrinho.items.push({ produto_id:req.params.produto_id, qtde: 1 });
+    }
+  }
+  res.render('carrinho/index', { produto: req.params.produto_id });
 });
 
 
