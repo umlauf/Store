1a3cf8ae799a578db569a0ada649d511488b204b
Sérgio Umlauf <sergio.umlauf@mosaicco.com>
2015-12-09 16:05:18 -0200

    Implementação inicial do carrinho

--- routes/carrinho.js
+++ routes/carrinho.js
@@ -1,28 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Produto = mongoose.model('produtos');
+var Price = require('format-price');
 
 
 router.get('/', function(req, res, next) {
+  if(req.session.carrinho && req.session.carrinho.items) {
+    var predicate = [];
+    req.session.carrinho.items.forEach(function(item) {
+      predicate.push(item.produto_id);
+      console.log(item.produto_id);
+    });
 
-  res.render('carrinho/index');
-
+    Produto.find({ _id: { $in: predicate } }, function(err, produtos){
+      // TODO: melhorar esse loop dentro de loop.
+      if(produtos) {
+        console.log("ok");
+        produtos.forEach(function(produto) {
+          req.session.carrinho.items.forEach(function(item) {
+            if(produto._id == item.produto_id) {
+              produto.qtde = item.qtde;
+              produto.valorTotalFormatado = Price.format( 'pt-BR', 'BRL', produto.valor * produto.qtde);
+            }
+          });
+        });
+      }
+      res.render(
+        'carrinho/index',
+        {title : "Produtos", items : produtos}
+      );
+    });
+  } else {
+    res.render(
+      'carrinho/index',
+      {title : "Produtos", items : []}
+    );
+  }
 });
 
 
 router.get('/:produto_id', function(req, res, next) {
   if(!req.session.carrinho) {
-    req.session.carrinho = { items:[] };
-    req.session.carrinho.items.push({ produto_id:req.params.produto_id, qtde: 1 });
+    req.session.carrinho = { items: [] };
+    req.session.carrinho.items.push({ produto_id: req.params.produto_id, qtde: 1 });
   } else {
     // Só adiciona o produto se ainda não existir no carrinho.
     var existe = req.session.carrinho.items.some(function(item) {
       return item.produto_id == req.params.produto_id;
     });
     if(!existe) {
-      req.session.carrinho.items.push({ produto_id:req.params.produto_id, qtde: 1 });
+      req.session.carrinho.items.push({ produto_id: req.params.produto_id, qtde: 1 });
     }
   }
-  res.render('carrinho/index', { produto: req.params.produto_id });
+  res.redirect('/carrinho');
 });
 
 
